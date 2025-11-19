c04aa713a9ca657579178a5d4b0eb0d6c27df0ec
Obsidian-fun <obsidiancrystal@tutanota.com>
2025-11-19 18:41:07 +0000

    modified:   main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,17 +11,34 @@
 using namespace std;
 
 static struct {
+	sg_pipeline pip; 
+	sg_bindings bind;
 	sg_pass_action pass_action;
 } state;
 
-void init (void) {
-	// make sg report errors, warnings and such,
+static void init (void) {
 	sg_desc desc = {
 		.logger = {.func = slog_func},
 		.environment = sglue_environment()
 	};
-				
 	sg_setup(&desc);
+
+ /* create shader from code-generated sg_shader_desc */
+	sg_shader shd = sg_make_shader(simple_shader_desc(sg_query_backend()));
+
+ /* a vertex buffer with 3 vertices */
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f, // bottom left
+		0.5f, -0.5f, 0.0f,		// bottom right
+		0.0f, 0.5f, 0.0f		// top 
+	}
+
+	state.bind.vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc) {
+		.size = sizeof(vertices),
+		.data = SG_RANGE(vertices),
+		.label = "triangle_vertices"
+	});
+
 
 	state.pass_action = (sg_pass_action){};
 	state.pass_action.colors[0].load_action = SG_LOADACTION_CLEAR;
