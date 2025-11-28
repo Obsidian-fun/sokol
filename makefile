81e8613c2896ec4af98e667ab3d36f93a10b67c1
Obsidian-fun <obsidiancrystal@tutanota.com>
2025-11-28 16:45:20 +0000

    modified:   ../makefile 	new file:   header/2-quad.glsl.h 	modified:   main.cpp

--- makefile
+++ makefile
@@ -1,11 +1,12 @@
 CC = g++
 CFLAGS = -Wall
 LDFLAGS = -lGL -lm -ldl -lpthread -pthread -lX11 -lXcursor -lXi
+INCLUDES = -I/src/header 
 SRC = src/main.cpp
 OUT = build/sokol_app
 
 all:
-	$(CC) $(SRC) -o $(OUT) $(CFLAGS) $(LDFLAGS)
+	$(CC) $(INCLUDES) $(SRC) -o $(OUT) $(CFLAGS) $(LDFLAGS)
 	$(info $$DATA = $(DATA))
 
 clean:
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-/* stock ticker - bullish OHLC */
+/* shaders - in and out , part 1*/
 #define SOKOL_IMPL
 #define SOKOL_GFX_IMPL
 #define SOKOL_GLCORE
@@ -7,17 +7,11 @@
 #include "header/sokol_gfx.h"
 #include "header/sokol_glue.h"
 #include "header/sokol_log.h"
-#include "header/2-quad.glsl.h"
+#include "header/1-in-out.glsl.h"
 
-/***
-creating 2 pipelines for quadrilateral and 2 lines. Each pipeline needs it's own binding for index_data in
-the buffer object. So there will be 2 draw calls, one for a quad and one for 2 lines.
-***/
 static struct {
-	sg_pipeline pip_quad;
-	sg_pipeline pip_line;
-	sg_bindings bind_quad;
-	sg_bindings bind_line;
+	sg_pipeline pip;
+	sg_bindings bind;
 	sg_pass_action pass_action;
 } state;
 
@@ -29,49 +23,18 @@ static void init (void) {
 	sg_setup(&desc);
 	sg_shader shd = sg_make_shader(simple_shader_desc(sg_query_backend()));	
 	float vertices[] = {
-		-0.25f, -0.5f, 0.0f,	// 0: Open - left corner
-		0.25f, -0.5f, 0.0f,		// 1: Open - righet corner
-		-0.25f, 0.5f, 0.0f,		// 2: Close - left corner
-		0.25f, 0.5f,	0.0f,		// 3: Close - right corner
-		0.0f, 0.7f, 0.0f, 		// 4: High 
-		0.0f, -0.7f, 0.0f 		// 5: Low
+	 // positions
+    -0.5f, -0.5f, 0.0f,     // bottom left
+    0.5f, -0.5f, 0.0f,      // bottom right
+    0.0f,  0.5f, 0.0f       // top
 	};
 	sg_buffer_desc buffer_desc = { // loading up vertex data from buffer object
 		.size = sizeof(vertices),
 		.data = SG_RANGE(vertices),
-		.label = "ticker_vertices"
+		.label = "triangle_vertices"
 	};
-	state.bind_quad.vertex_buffers[0] = sg_make_buffer(&buffer_desc);
-	state.bind_line.vertex_buffers[0] = sg_make_buffer(&buffer_desc);
-	uint16_t indices_quad[] = {
-		0, 1, 2, // Left triangle
-		2, 3, 1  // Right triangle
-	};
-	uint16_t indices_line[] = {
-		4,5,	// high to low point for the day
-	};
-	buffer_desc = {							// loading up index data from buffer object
-		.size = sizeof(indices_quad),
-		.usage = {
-			.index_buffer = true,
-			.immutable = true
-		},	
-		.data = SG_RANGE(indices_quad),
-		.label = "quad_indices"
-	};
-	state.bind_quad.index_buffer = sg_make_buffer(&buffer_desc);
-	buffer_desc = {	
-		.size = sizeof(indices_line),
-		.usage = {
-			.index_buffer = true,
-			.immutable = true
-		},	
-		.data = SG_RANGE(indices_line),
-		.label = "line_indices"
-	};
-	state.bind_line.index_buffer = sg_make_buffer(&buffer_desc);
+	state.bind.vertex_buffers[0] = sg_make_buffer(&buffer_desc);
 
-	// 2 pipeline structs,
 	sg_pipeline_desc pipeline_desc = {
 		.shader = shd,
 		.layout = {
@@ -81,22 +44,9 @@ static void init (void) {
 		},
 		.primitive_type = SG_PRIMITIVETYPE_TRIANGLES,
 		.index_type = SG_INDEXTYPE_UINT16,
-		.label = "quad_position"
-	};
-	state.pip_quad = sg_make_pipeline(&pipeline_desc); 
-	pipeline_desc = {
-		.shader = shd,
-		.layout = {
-			.attrs = {
-				[0] = {.format = SG_VERTEXFORMAT_FLOAT3}
-			}
-		},
-		.primitive_type = SG_PRIMITIVETYPE_LINES,
-		.index_type = SG_INDEXTYPE_UINT16,
-		.label = "line_position"
+		.label = "triangle_position"
 	};
 	state.pip_line = sg_make_pipeline(&pipeline_desc); 
-
 	state.pass_action = (sg_pass_action){};
 	state.pass_action.colors[0].load_action = SG_LOADACTION_CLEAR;
 	state.pass_action.colors[0].clear_value = {0.2f, 0.3f, 0.3f, 1.0f};
@@ -111,11 +61,7 @@ void frame(void) {
 
 	sg_apply_pipeline(state.pip_quad);
 	sg_apply_bindings(&state.bind_quad);
-	sg_draw(0, 6, 1);
-
-	sg_apply_pipeline(state.pip_line);
-	sg_apply_bindings(&state.bind_line);
-	sg_draw(0, 2, 1);
+	sg_draw(0, 3, 1);
 
 	sg_end_pass();
 	sg_commit();
