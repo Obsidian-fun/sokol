bcea8ec21028d64522fdd2a5a36fa4274643775e
Obsidian-fun <obsidiancrystal@tutanota.com>
2025-11-29 18:43:10 +0000

    modified:   2-uniforms-translate.glsl

--- src/2-uniforms-translate.glsl
+++ src/2-uniforms-translate.glsl
@@ -4,24 +4,23 @@ uniforms are global variable they need to be manually reset otherwise they carry
 
 @vs vs
 in vec4 position;
+layout (binding = 0) uniform fs_params {
+	vec4 ourPostion; 
+};
 
 void main() {
 	gl_Position = vec4(position.x, position.y, position.z, 1.0);
+	gl_Position = ourPosition;
 }
 @end
 
 @fs fs
-layout (binding = 0) uniform fs_params {
-	vec4 ourColor; // the properties of ourColor carry from the main program
-};
-
 out vec4 FragColor; 
 
 void main() {
 	FragColor = ourColor;
 }
 @end
-
 
 @program simple vs fs
 
