5e6135a1b88705a05e3c7a9ef68273234846c792
Obsidian-fun <obsidiancrystal@tutanota.com>
2025-12-03 15:01:30 +0000

    modified:   src/main.cpp 	new file:   tutorial/rotate_scale/main.cpp 	new file:   tutorial/rotate_scale/transformations.glsl

---
+++ tutorial/rotate_scale/transformations.glsl
@@ -0,0 +1,26 @@
+@ctype mat4 HMM_Mat4 
+
+@vs vs
+in vec3 position;
+
+layout (binding=0) uniform vs_params {
+	mat4 transform;
+};
+
+void main() {
+	gl_Position = transform * vec4(position, 1.0);
+}
+@end
+
+@fs fs
+out vec4 FragColor;
+
+void main() {
+	FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
+}
+@end
+
+@program simple vs fs
+
+
+
