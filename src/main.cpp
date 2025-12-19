c02ccced5dc8c0202698794f9f5ccf13038997eb
Obsidian-fun <obsidiancrystal@tutanota.com>
2025-12-19 05:22:45 +0000

    modified:   src/main.cpp 	deleted:    src/shader/1-in-out.glsl

--- src/main.cpp
+++ src/main.cpp
@@ -1,105 +1,76 @@
-/* instanced arrays */
-#define HANDMADE_MATH_IMPLEMENTATION
-#include "header/HandmadeMath.h"
-
+/* Renders a triangle */
 #define SOKOL_IMPL
 #define SOKOL_GFX_IMPL
 #define SOKOL_GLCORE
 
-#include "header/sokol_app.h"
-#include "header/sokol_gfx.h"
-#include "header/sokol_glue.h"
-#include "header/sokol_log.h"
-#include "header/2-instanced-arrays.glsl.h"
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#include "sokol_glue.h"
+#include "sokol_log.h"
+#include "1-triangle.glsl.h"
+
+using namespace std;
 
 static struct {
-	sg_pipeline pip;
+	sg_pipeline pip; 
 	sg_bindings bind;
 	sg_pass_action pass_action;
 } state;
 
 static void init (void) {
-
 	sg_desc desc = {
 		.logger = {.func = slog_func},
 		.environment = sglue_environment()
 	};
 	sg_setup(&desc);
 
-	sg_shader shd = sg_make_shader(simple_shader_desc(sg_query_backend()));	
-	float vertices[] = {
-	 /* positions         	*/
-    -0.05f, 0.05f,    // top left
-    0.05f, -0.05f,    // bottom right
-    -0.05f,  -0.05f,	// bottom left
+ /* create shader from code-generated sg_shader_desc */
+	sg_shader shd = sg_make_shader(simple_shader_desc(sg_query_backend()));
 
-		-0.05f, 0.05f, 		// top left
-		0.05f, -0.05f, 		// bottom right
-    0.05f,  0.05f   	// top right
+ /* a vertex buffer with 3 vertices */
+	float vertices[] = {
+		/*postion */						
+		-0.5f,-0.5f, 0.0f, 	// bottom left
+		0.5f, -0.5f, 0.0f,	// bottom right
+		0.0f, 0.5f, 0.0f		// top 
 	};
 
-	sg_buffer_desc buffer_desc = { // loading up vertex data into vertex buffer object (VBO)
+	sg_buffer_desc buffer_description = {
 		.size = sizeof(vertices),
 		.data = SG_RANGE(vertices),
-		.label = "quad_vertices"
+		.label = "triangle_vertices"
 	};
-	state.bind.vertex_buffers[0] = sg_make_buffer(&buffer_desc);
-
-	// load offset data directly into vertex buffer object (VBO)
-	HMM_Vec2 translations[100];
-	int index = 0; 
-	float offset = 0.1f;
-	for (int y=-10; y<10; y+=2) {
-		for (int x=-10; x<10; x+=2) {
-			float x_pos = (float)x /10.0f + offset;
-			float y_pos = (float)y /10.0f + offset;
-			translations[index++] = HMM_V2(x_pos, y_pos);
-		}
-	}
-	buffer_desc = {}; // reset buffer struct
-	buffer_desc = {
-		.size = sizeof(translations),
-		.data = SG_RANGE(translations),
-		.label = "offsets"
-	};
-	state.bind.vertex_buffers[1] = sg_make_buffer(&buffer_desc); 
-
-	sg_pipeline_desc pipeline_desc = {
+	state.bind.vertex_buffers[0] = sg_make_buffer(&buffer_description);
+	/*create a pipeline object (default render states are fine for triangle) */
+	sg_pipeline_desc pipeline_description =  {
 		.shader = shd,
+			/*if the vertex layout doesn't have gaps, don't need to provide strides and offsets */
 		.layout = {
-			.buffers = {
-				[0] = {.stride = 0, .step_func = SG_VERTEXSTEP_PER_VERTEX, .step_rate = 1},
-				[1] = {.stride = 0, .step_func = SG_VERTEXSTEP_PER_INSTANCE, .step_rate = 1}
-			},
 			.attrs = {
-				[0] = {.buffer_index = 0, .offset = 0, .format = SG_VERTEXFORMAT_FLOAT2},
-				[1] = {.buffer_index = 1, .offset = 0, .format = SG_VERTEXFORMAT_FLOAT2}
+				[0] = {.format = SG_VERTEXFORMAT_FLOAT3 }
 			}
 		},
-		.primitive_type = SG_PRIMITIVETYPE_TRIANGLES,
-		.label = "quad_pipeline"
+		.label = "triangle-pipeline"
 	};
-	state.pip = sg_make_pipeline(&pipeline_desc); 
+	state.pip = sg_make_pipeline(&pipeline_description);
 	state.pass_action = (sg_pass_action){};
 	state.pass_action.colors[0].load_action = SG_LOADACTION_CLEAR;
 	state.pass_action.colors[0].clear_value = {0.2f, 0.3f, 0.3f, 1.0f};
 }
 
-void frame(void) {
-
-	sg_pass pass {
+void frame (void) {
+	sg_pass pass = {
 		.action = state.pass_action,
 		.swapchain = sglue_swapchain()
 	};
-	sg_begin_pass(&pass);
+	sg_begin_pass(&pass); 
 	sg_apply_pipeline(state.pip);
 	sg_apply_bindings(&state.bind);
-
-	sg_draw(0, 6, 100);
+	sg_draw(0,3,1); // sg_draw(int base_element, int num_elements, int num_instances) 
 	sg_end_pass();
 	sg_commit();
 }
-	
+
 void cleanup(void) {
 	sg_shutdown();
 }
@@ -113,17 +84,15 @@ void event(const sapp_event* e) {
 }
 
 sapp_desc sokol_main(int argc, char *argv[]) {
-  return (sapp_desc) {
-    .init_cb = init,
-    .frame_cb = frame,
-    .cleanup_cb = cleanup,
-    .event_cb = event,
-    .width = 800,
-    .height = 600,
-    .high_dpi = true,
-    .window_title = "Instancing..."
-  };
+	return (sapp_desc) {
+		.init_cb = init,
+		.frame_cb = frame,
+		.cleanup_cb = cleanup,
+		.event_cb = event,
+		.width = 800,
+		.height = 600,
+		.high_dpi = true,
+		.window_title = "First Triangle"
+	};
 }
-
-
 
